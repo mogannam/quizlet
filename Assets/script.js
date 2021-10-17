d3388c4461f158538f9bd40f41ea552253604eb1
mogannam <JoeMogannam@gmail.com>
2021-10-16 19:43:12 -0700

    I walked away from the computer, saving work

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,13 @@
 var bool_dbg = true;
 // end the quiz if all questions answered or times up
 var bool_endQuiz = false; 
-var int_secondsWait = 500;
+var int_secondsWait = 800;
 if(bool_dbg)console.log("debugging")
 var int_time = 60;
 var int_highScore = 0;
 var int_answerCounter = 0;
 var int_questionCounter= 0;
+var str_userAnswered = "";
 // create a dictionary of arrays
 // the key is the users initials
 // the value is an array of thier scores as an object
@@ -16,7 +17,7 @@ var obj_oneScore = { str_Initials:"NULL", int_score:0, int_time:0 };
 
 
 // create an array of question object objects
-var arr_questions = [
+var arrObj_questions = [
     {str_answer: "insertCorrectAnswer", str_question: "insertQuestion1", int_points:10, correctAnswer: "InsertOption1", ArrStr_answerOptions: ["InsertOption1","InsertOption2", "InsertOption3"]},
     {str_answer: "insertCorrectAnswer", str_question: "insertQuestion2", int_points:20, correctAnswer: "InsertOptionB", ArrStr_answerOptions: ["InsertOptionA","InsertOptionB", "InsertOptionC"]},
     {str_answer: "insertCorrectAnswer", str_question: "insertQuestion3", int_points:30, correctAnswer: "InsertOption5", ArrStr_answerOptions: ["InsertOption4","InsertOption5", "InsertOption6"]}
@@ -39,8 +40,138 @@ var func_waitnSeconds = function(paramSeconds){
 
 }
 
+var func_checkAnswer = function(){
+    var bool_validAnswer = false;
+    // ToDo
+    var divElement_msg = document.querySelector("#form-question-msg");
+    // get the current question 
+    var obj_currentQuestion = arrObj_questions[int_questionCounter]
+    var str_currentAnswer = obj_currentQuestion.str_answer
+
+    // todo add css for wrong /correct color coding
+    //todo update class namews with a class name of correct or wrong
+    // todo update the message to color code correct answer
+    // todo update if statement to color code wrong answer & correcct answer in msg
+    // todo adjust score
+
+    if(str_userAnswered === str_currentAnswer )
+        bool_validAnswer = true
+    else
+        bool_validAnswer = false
+    
+
+    var pElement = document.createElement("p");
+    pElement.className = ""
+    divElement_msg.textContent = `Testing ${int_questionCounter}`
+    bool_validAnswer = false
+}
+
+var func_nextQuestion = function (){
+     // create a div place holder that represents
+    // quiz-dynamic-section div already on index.html
+
+    // clear the message, for the previous question
+    var divElement_msg = document.querySelector("#form-question-msg");
+    divElement_msg.textContent = ``;
+
+    if(bool_dbg)console.log(`counter ${int_questionCounter} , bool_endQui ${bool_endQuiz}`)
+
+    // if we run out of time or run out of questions 
+    // then end the quiz
+    // ToDO: end quiz if time runs out
+    if(int_questionCounter >= arrObj_questions.length){
+        bool_endQuiz = true;
+        func_endQuiz();
+        return;
+    }
+    
+
+    var divElement = document.createElement("div");
+    divElement.className = "quiz-dynamic-section";
+
+    // start the game by asking the first question
+    // add the first question to index.html dynamically
+    var obj_currentQuestion = arrObj_questions[int_questionCounter]
+    var ArrStr_answerOptions= obj_currentQuestion.ArrStr_answerOptions;
+    // parse my json object into an html friendly string
+    var str_question = JSON.stringify(obj_currentQuestion.str_question);
+    str_question = JSON.parse(str_question)
+
+    // create the html code for the questions
+    // each question will be an individual form
+    // the form will have a label tag, select tag with multiple options
+    var formElement = document.createElement("form");
+    formElement.className = "form-question";
+
+    // the question is added to index.html as a label
+    var labelElement = document.createElement("label");
+    labelElement.className = "form-question label-question";
+    // add the question text to the label
+    labelElement.textContent = str_question;
+    var selectElement = document.createElement("select");
+    selectElement.className = 'form-question select-question';
+    // force the drop down to expand to a size equal to the number 
+    // of answers for this question. This forces the options not to appear
+    // in  a drop down
+    selectElement.size=ArrStr_answerOptions.length;
+
+    //loop through all the options available as an answer
+    // create an option for every possible answer
+    for(var i =0; i < ArrStr_answerOptions.length; i++){
+        var optionElement = document.createElement("option")
+        // set an option element to the value of the ith answer
+        var str_currentAnswer = ArrStr_answerOptions[i];
+        optionElement.value = str_currentAnswer;
+        optionElement.textContent = str_currentAnswer;
+        optionElement.className = "form-question question-option"
+        // set a unique id across all possible answers across all questions
+        optionElement.id = "answer-id-" + int_answerCounter;
+        // add a custom data attribute to the options element
+        optionElement.setAttribute("data-answer-id",int_answerCounter)
+        // add the ith option element to the select element
+        selectElement.appendChild(optionElement);
+        int_answerCounter++;
+    }
+
+   
+
+    // add all the dynamic elements to the final div so they are 
+    // actually rendered
+
+    // add the label to the form 
+    formElement.appendChild(labelElement);
+    //add the select to the form
+    formElement.appendChild(selectElement);
+
+    // add the form to our place holder div
+    divElement.appendChild(formElement);
+  
+
+    //divElement.innerHTML = `<h1> ${obj_currentQuestion.str_question} </h1>`;
+    // Set the div on index.html to our placeholder div to render the changes
+    divElement_DynamicSection.innerHTML =  divElement.innerHTML;
+    int_questionCounter++;
+}
+
+var func_startQuiz = function (){
+    //update dynamic section with a new section
+    //ToDo: clear global variables for when the quiz restarts
+ 
+    int_questionCounter = 0
+    bool_endQuiz = false;
+    // todo: set timer to zero
+    // set high score hack to zero
+    int_highScore = 0;
+
+    if(bool_dbg)console.log('starting quiz');
+    func_nextQuestion();
+
+    
+}
+
+
 var func_endQuiz = function(){
-    // ToDo: add current score to list of high scores
+    
     var divElement = document.createElement("div");
     divElement.className = "quiz-dynamic-section";
     
@@ -61,18 +192,19 @@ var func_endQuiz = function(){
     var inputElement = document.createElement("input");
     inputElement.className = "end-quiz";
     var btnElement_submit = document.createElement("button")
-    btnElement_submit.className = "end-quiz btn-end-quiz"
-    btnElement_submit.id = "btn-end-quiz"    
+    btnElement_submit.className = "end-quiz btn-submit-initials"
+    btnElement_submit.id = "btn-submit-initials"    
     btnElement_submit.textContent = "submit! "
     divElement.appendChild(h1Element)
     divElement.appendChild(pElement)
     
-    //todo: add input to the label
+    //add the input element to the label so they apear side by side
     labelElement.appendChild(inputElement)
-    //add label to form
+
+    //add the label to theform
     formElement.appendChild(labelElement)
     
-    // todo add button
+    // add the submit button to the form
     formElement.appendChild(btnElement_submit)
     
     // add the form to our place holder div
@@ -83,96 +215,43 @@ var func_endQuiz = function(){
 
 }
 
-var func_nextQuestion = function (){
-     // create a div place holder that represents
-    // quiz-dynamic-section div already on index.html
-    if(bool_dbg)console.log(`counter ${int_questionCounter} , bool_endQui ${bool_endQuiz}`)
 
-    // if we run out of time or run out of questions 
-    // then end the quiz
-    // ToDO: end quiz if time runs out
-    if(int_questionCounter >= arr_questions.length){
-        bool_endQuiz = true;
-        func_endQuiz();
-        return;
-    }
+
+
+var func_resetGame = function () {
+    
+     
+     var divElement = document.createElement("div");
+     divElement.className = "quiz-dynamic-section";
+     
+     var h1Element = document.createElement("h1")
+     h1Element.textContent = "High Scores! <br> Place holder remider to add ol with scores"
+     h1Element.className ="reset-quiz"
+
+     var btnElement_goBack = document.createElement("button")
+     btnElement_goBack.className = "reset-quiz btn-go-back";
+     btnElement_goBack.id = "btn-go-back";
+     btnElement_goBack.textContent = "Go Back"
+
+     var btnElement_clearScore = document.createElement("button")
+     btnElement_clearScore.className = "reset-quiz btn-clear-score";
+     btnElement_clearScore.id = "btn-clear-score"
+     btnElement_clearScore.textContent = "Clear High Scores"
+
+     divElement.appendChild(h1Element);
+     // ToDo: list all scores in local cache
+    // create a temp div to hold our buttons in one element
+    var divElement_buttons = document.createElement("div")
+    divElement_buttons.appendChild(btnElement_goBack);
+    divElement_buttons.appendChild(btnElement_clearScore)
+
+    divElement.appendChild(divElement_buttons);
     
 
-    var divElement = document.createElement("div");
-    divElement.className = "quiz-dynamic-section";
-
-    // start the game by asking the first question
-    // add the first question to index.html dynamically
-    var obj_currentQuestion = arr_questions[int_questionCounter]
-    var ArrStr_answerOptions= obj_currentQuestion.ArrStr_answerOptions;
-    // parse my json object into an html friendly string
-    var str_question = JSON.stringify(obj_currentQuestion.str_question);
-    str_question = JSON.parse(str_question)
-
-    // create the html code for the questions
-    // each question will be an individual form
-    // the form will have a label tag, select tag with multiple options
-    var formElement = document.createElement("form");
-    formElement.className = "form-question";
-
-    // the question is added to index.html as a label
-    var labelElement = document.createElement("label");
-    labelElement.className = "label-question";
-    // add the question text to the label
-    labelElement.textContent = str_question;
-    var selectElement = document.createElement("select");
-    selectElement.className = 'select-question';
-    // force the drop down to expand to a size equal to the number 
-    // of answers for this question. This forces the options not to appear
-    // in  a drop down
-    selectElement.size=ArrStr_answerOptions.length;
-
-    //loop through all the options available as an answer
-    // create an option for every possible answer
-    for(var i =0; i < ArrStr_answerOptions.length; i++){
-        var optionElement = document.createElement("option")
-        // set an option element to the value of the ith answer
-        var str_currentAnswer = ArrStr_answerOptions[i];
-        optionElement.value = str_currentAnswer;
-        optionElement.textContent = str_currentAnswer;
-        optionElement.className = "question-option"
-        // set a unique id across all possible answers across all questions
-        optionElement.id = "answer-id-" + int_answerCounter;
-        // add a custom data attribute to the options element
-        optionElement.setAttribute("data-answer-id",int_answerCounter)
-        // add the ith option element to the select element
-        selectElement.appendChild(optionElement);
-        int_answerCounter++;
-    }
-
-    // add the label to the form 
-    formElement.appendChild(labelElement);
-    //add the select to the form
-    formElement.appendChild(selectElement);
-
-    // add the form to our place holder div
-    divElement.appendChild(formElement);
-    //divElement.innerHTML = `<h1> ${obj_currentQuestion.str_question} </h1>`;
     // Set the div on index.html to our placeholder div to render the changes
     divElement_DynamicSection.innerHTML =  divElement.innerHTML;
-    int_questionCounter++;
-}
 
-var func_startQuiz = function (){
-    //update dynamic section with a new section
-    //ToDo: clear global variables for when the quiz restarts
-    int_questionCounter = 0
-    bool_endQuiz = false;
-    // todo: set timer to zero
-    // todo: set score to zero
-    if(bool_dbg)console.log('starting quiz');
-    func_nextQuestion();
-
-    
-}
-
-var func_checkAnswer = function(){
-    // ToDo
+     
 }
 
 var buttonHandler =function(event){
@@ -196,14 +275,25 @@ var buttonHandler =function(event){
         // then move on
 
         // ToDo check current answer
-        // wait a few seconds
-        //func_waitnSeconds(10000)
-        setTimeout(func_nextQuestion, int_secondsWait)
 
-        // ToDo adjust score
-        // ToDo move to next question
+        str_userAnswered = event.target.value;
+        if(bool_dbg)console.log(`User selected option : ${str_userAnswered}`)
+        func_checkAnswer();
+        // wait a few seconds, call next question
+        setTimeout(func_nextQuestion, int_secondsWait)
         
 
+        // ToDo adjust score
+        str_userAnswered = ""
+    }
+    else if (event.target.matches(".btn-submit-initials")) { 
+        // users submited their initals and ended the quiz
+        // listens on the submit button dynamically added by func_endQuiz
+        func_resetGame();
+    }
+    else
+    {
+        // do nothing
     }
 
 };
